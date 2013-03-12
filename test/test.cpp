850118376f2854146836dce6aa8f99f2e21d2883
David Eliahu <davideliahu@gmail.com>
2013-03-11 21:17:57 -0700

    add test file

---
+++ test/test.cpp
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <float.h>
+#include <math.h>
+#include <time.h>
+#include <sys/time.h>
+#include <mkl.h>
+using namespace std;
+// #include <cilk/cilk.h>
+// #include <cilk/cilk_api.h>
+
+class TRSMProblem {
+public:
+    void printStuff() {
+        printf("this is TRSM\n");
+    }
+};
+
+class MultProblem {
+public:
+    void printStuff() {
+        printf("this is Mult\n");
+    }
+};
+
+class TRSMResult {
+public:
+    int x;
+    void printStuff() {
+        printf("this is TRSM Result. x = %d\n", x);
+    }
+};
+
+class MultResult {
+public:
+    int y;
+    void printStuff() {
+        printf("this is Mult Result. y = %d\n", y);
+    }
+};
+
+TRSMResult solveBase(TRSMProblem p) {
+    // printf("getting TRSM subproblems\n");
+    TRSMResult result;
+    result.x = 19;
+    return result;
+}
+
+MultResult solveBase(MultProblem p) {
+    // printf("getting Mult subproblems\n");
+    MultResult result;
+    result.y = 17;
+    return result;
+}
+
+template <class Problem, class Result>
+Result solve (Problem problem) {
+    Result result = solveBase(problem);
+    return result;
+}
+
+int main () {
+    TRSMProblem trsmProb;
+    MultProblem multProb;
+    TRSMResult trsmResult = solve<TRSMProblem, TRSMResult>(trsmProb);
+    MultResult multResult = solve<MultProblem, MultResult>(multProb);
+    trsmResult.printStuff();
+    multResult.printStuff();
+    return 0;
+}
