759d113c891b533d9dc98b5d1d314515a22fe5a5
David Eliahu <davideliahu@gmail.com>
2013-12-02 12:59:30 -0800

    simplify mergesort

--- algorithms/mergesort/MergesortProblem.cpp
+++ algorithms/mergesort/MergesortProblem.cpp
@@ -23,43 +23,37 @@ void MergesortProblem::runBaseCase() {
 
 std::vector<Task*> MergesortProblem::split() {
     int midpoint = length/2;
-    Task* task1 = new Task(new MergesortProblem(A, midpoint));
-    Task* task2 = new Task(new MergesortProblem(A + midpoint, midpoint + (length % 2)));
-
     std::vector<Task*> tasks (2);
-    tasks[0] = task1;
-    tasks[1] = task2;
+    tasks[0] = new Task(new MergesortProblem(A, midpoint));
+    tasks[1] = new Task(new MergesortProblem(A + midpoint, midpoint + (length % 2)));
     return tasks;
 }
 
 void MergesortProblem::merge(std::vector<Problem*> subproblems) {
-    MergesortProblem* r1 = static_cast<MergesortProblem*>(subproblems[0]);
-    MergesortProblem* r2 = static_cast<MergesortProblem*>(subproblems[1]);
+    MergesortProblem* s1 = (MergesortProblem*) subproblems[0];
+    MergesortProblem* s2 = (MergesortProblem*) subproblems[1];
 
-    int r1Length = r1->length;
-    int r2Length = r2->length;
-    double *r1A = r1->A;
-    double *r2A = r2->A;
     double *merged = (double*) malloc(length * sizeof(double));
     int p1 = 0, p2 = 0, i = 0;
 
     // merge
-    while (p1 != r1Length && p2 != r2Length) {
-        if (r1A[p1] < r2A[p2]) {
-            merged[i] = r1A[p1];
+    while (p1 != s1->length && p2 != s2->length) {
+        if (s1->A[p1] < s2->A[p2]) {
+            merged[i] = s1->A[p1];
             p1++;
         } else {
-            merged[i] = r2A[p2];
+            merged[i] = s2->A[p2];
             p2++;
         }
         i++;
     }
 
-    // move leftovers if  p1 did not finish
-    if (p2 == r2Length) {
-        memcpy(A+i, r1A+p1, (length-i) * sizeof(double));
+    // move leftovers
+    if (p2 == s2->length) {
+        memcpy(A+i, s1->A+p1, (length-i) * sizeof(double));
     }
 
+    // copy merged array into A
     memcpy(A, merged, i * sizeof(double));
 
     free(merged);
