da51bb56f7ad8740d3213aa5a7281cabc358998e
David Eliahu <davideliahu@gmail.com>
2013-12-03 23:48:24 -0800

    fix tune.sh

--- tune.sh
+++ tune.sh
@@ -6,7 +6,7 @@ export CILK_NWORKERS=32
 export MKL_NUM_THREADS=1
 echo -e "\e[01;34mRunning with $CILK_NWORKERS threads\e[0m"
 
-OPENTUNERPATH="/home/eecs/deliahu/opentuner/"
+OPENTUNERPATH="$HOME/opentuner"
 export PYTHONPATH=$PYTHONPATH:$OPENTUNERPATH
 otpython=$OPENTUNERPATH/venv/bin/python
 
@@ -20,7 +20,7 @@ FRAMEWORK="framework/framework.cpp framework/Task.cpp framework/Problem.cpp fram
 # STRASSEN
 icc $FLAGS -I framework -o harness algorithms/strassen/*.cpp $FRAMEWORK
 dim=1024
-$otpython algorithms/strassen/strassen_tune.py --test-limit 10 --max_depth 5 --m $dim --k $dim --n $dim
+$otpython algorithms/strassen/strassen_tuner.py --test-limit 10 --max_depth 5 --m $dim --k $dim --n $dim
 
 rm -rf harness
 echo -e "\e[01;32mTuning took" $SECONDS "seconds\e[0m"
