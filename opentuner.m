863dc5de56a905f73539bf32e16d027b206f9b8f
David Eliahu <davideliahu@gmail.com>
2013-12-06 14:02:40 -0800

    improve opentuner.m

--- opentuner.m
+++ opentuner.m
@@ -1,8 +1,10 @@
 clear;
 
+
 peak = 124.9512;
 y_plot_round = 10;
 
+legend_font_size = 17;
 tick_label_size = 15;
 axis_label_size = 21;
 % my_axis = [0 4700 0 4700];
@@ -10,42 +12,60 @@ axis_label_size = 21;
 line_width = 1.5;
 peak_color = [0,.5,0];
 
-infile = 'opentuner.csv';
-data = csvread(infile);
-
-data_adjusted = [];
-for i = 1:size(data,1)
-    row = data(i,:);
-    cur_max = row(1);
-    row_adjusted = [cur_max];
-    for j = 2:length(row)
-        cur_max = max([cur_max, row(j)]);
-        row_adjusted = [row_adjusted, cur_max];
+infiles = {};
+allFiles = dir('data/opentuner/');
+for k = 1 : length(allFiles)
+    if(strfind(allFiles(k).name, '.csv'))
+        if(strfind(allFiles(k).name, 'opentuner'))
+            infiles{end+1} = ['data/opentuner/', allFiles(k).name];
+        end
     end
-    data_adjusted = [data_adjusted; row_adjusted];
 end
 
-Xs = 1:size(data,2);
-Ys = median(data_adjusted);
-first_quart = quantile(data_adjusted,.25);
-third_quart = quantile(data_adjusted,.75);
+for infile = infiles
+    infile = infile{1};
+    [pathstr,infilename,ext] = fileparts(infile);
+    display(['running ', infilename]);
+    data = csvread(infile);
+    num_cols_to_add = 100 - size(data,2);
+    data = [data, zeros(size(data,1), num_cols_to_add)];
+    data_adjusted = [];
+    for i = 1:size(data,1)
+        row = data(i,:);
+        cur_max = row(1);
+        row_adjusted = [cur_max];
+        for j = 2:length(row)
+            cur_max = max([cur_max, row(j)]);
+            row_adjusted = [row_adjusted, cur_max];
+        end
+        data_adjusted = [data_adjusted; row_adjusted];
+    end
 
-fig = figure;
-plot([0, max(Xs)], [peak, peak], '--', 'Color', peak_color, 'LineWidth', line_width);
-hold on;
-plot(Xs, Ys, 'b', 'LineWidth', line_width);
-errorbar(Xs, Ys, Ys - first_quart, third_quart - Ys);
+    Xs = 1:100;
+    Ys = median(data_adjusted);
+    first_quart = quantile(data_adjusted,.25);
+    third_quart = quantile(data_adjusted,.75);
+    peak = max(max(data_adjusted));
 
-xlabel({'','Iteration'},'fontsize',axis_label_size);
-ylabel('Effective GFlops','fontsize',axis_label_size);
+    fig = figure;
+    plot([0, max(Xs)], [peak, peak], '--', 'Color', peak_color, 'LineWidth', line_width);
+    hold on;
+    plot(Xs, Ys, 'b', 'LineWidth', line_width);
+    errorbar(Xs, Ys, Ys - first_quart, third_quart - Ys);
 
-lh = legend({'Maximum Achievable Performance', 'Average Cumulative Performance'}, 'Location', 'southeast');
-legend('boxoff')
+    xlabel({'','Iteration'},'fontsize',axis_label_size);
+    ylabel('Effective GFlops','fontsize',axis_label_size);
 
-max_y_plot = y_plot_round*(ceil(peak/y_plot_round)); % round up to nearest y_plot_round
+    lh = legend({'Maximum Achievable Performance', 'Average Cumulative Performance'}, 'Location', 'southeast');
+    set(lh,'FontSize',legend_font_size);
+    legend('boxoff');
 
-axis([0 max(Xs) 0 max_y_plot]);
-% set(gca,'fontsize',tick_label_size,'xtick',[0:2000:max_x]);
+    max_y_plot = y_plot_round*(ceil(peak/y_plot_round)); % round up to nearest y_plot_round
 
-set(gca,'fontsize',tick_label_size);
-print(fig,'-depsc','opentuner.eps');
+    axis([0 max(Xs) 0 max_y_plot]);
+    % set(gca,'fontsize',tick_label_size,'xtick',[0:2000:max_x]);
+
+    set(gca,'fontsize',tick_label_size);
+    print(fig,'-depsc', [infilename, '.eps']);
+
+end
