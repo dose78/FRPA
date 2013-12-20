a99cbd75426441ded6eb4d6d06d8cbad58811cb5
David Eliahu <davideliahu@gmail.com>
2013-12-19 17:12:41 -0800

    fix loop bounds in papi script

--- papi_validation/run.sh
+++ papi_validation/run.sh
@@ -5,7 +5,7 @@ rm -rf *.csv
 rm -rf nohup.out
 
 # Run from 64KB to 100MB total memory
-for (( array_size=4096; array_size<6553600; array_size+=4096 )); do
+for (( array_size=4096; array_size<=6553600; array_size+=4096 )); do
     rm -f stream *.o
     icc -O2 -opt-streaming-stores never -openmp stream.c -DSTREAM_ARRAY_SIZE=$array_size /usr/local/lib/libpapi.a -o stream
     ./stream
