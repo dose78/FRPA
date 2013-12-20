7849e72225bcf81353478e4c779a7346ec548364
David Eliahu <davideliahu@gmail.com>
2013-12-19 17:06:37 -0800

    fix comment in papi script

--- papi_validation/run.sh
+++ papi_validation/run.sh
@@ -4,7 +4,7 @@ source /opt/intel/bin/iccvars.sh intel64
 rm -rf *.csv
 rm -rf nohup.out
 
-# Run from 64KB to 100MB
+# Run from 64KB to 100MB total memory
 for (( array_size=4096; array_size<6553600; array_size+=4096 )); do
     rm -f stream *.o
     icc -O2 -opt-streaming-stores never -openmp stream.c -DSTREAM_ARRAY_SIZE=$array_size /usr/local/lib/libpapi.a -o stream
